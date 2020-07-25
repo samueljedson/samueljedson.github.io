19bb01e3e9db7fa9212130616245675122af8d6a
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-25 15:16:51 -0700

    JS directions

--- functions.js
+++ functions.js
@@ -1,1 +1,12 @@
-window.onload = function sayHi() {alert(“Hello World of Javascript!”);
+//Commenting out alert
+//window.onload = function sayHi(){ alert(“Hello World of Javascript!”); } x = “goodbye world”  y=5
+//
+
+    var x, y;
+x = 5 + 6;
+y = x * 10;
+document.getElementById("first").innerHTML = y;
+
+function sayHi() {
+    var elemRef = document.getElementById("first");
+    elemRef.innerHTML = "goodbye";
