b5e177ba0a3189a007427af31e07b7892b351c4f
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-25 16:45:01 -0700

    button selectors

--- functions.js
+++ functions.js
@@ -11,12 +11,14 @@ document.getElementById("first").innerHTML = y;
 function sayHi() {
     var elemRef = document.getElementById("first");
     elemRef.innerHTML = "goodbye";
+
 }
 
 //reveals changes with button selection on my html and css files
 function myFunctionreveal() {
         document.getElementById("jsbutton").innerHTML = "Oceanside, Oregon";
+}   
     
     function myFunctionlook() {
         document.getElementById("jsbuttonstyle").style.color = "blue";
-      }
+}
