f12b0a4830f3a995ff3180e53117480f416da0ab
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-25 12:52:11 -0700

    JS update

--- functions.js
+++ functions.js
@@ -1,4 +1,27 @@
-window.onload = function sayHi(){ alert(“Hello World of Javascript!”); }
+//first attempt to alter pages via javascript using different parameters, variables and functions
+
+//window.onload = function sayHi() {
+    // 
+    // alert(“Hello World of Javascript!”); 
+ //}
+
+ //JS Entry or button
+    var x, y;
+x = 5 + 6;
+y = x * 10;
+document.getElementById("first").innerHTML = y;
+
 function sayHi() {
     var elemRef = document.getElementById("first");
-    ]
+    elemRef.innerHTML = "goodbye";
+
+}
+//adding a function to my index.html to reveal an answer
+
+function myFunctionreveal() {
+    document.getElementById("jsbutton").innerHTML = "Milan, Italy";
+  }
+
+  function myFunctionlook() {
+    document.getElementById("jsbuttonstyle").style.color = "blue";
+  }
