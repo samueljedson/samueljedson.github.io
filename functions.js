c05dad285165936a83df30944cb563dc3add744b
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-25 17:11:22 -0700

    function changes

--- functions.js
+++ functions.js
@@ -2,19 +2,8 @@
 //window.onload = function sayHi(){ alert(“Hello World of Javascript!”); } x = “goodbye world”  y=5
 //
 
-//change variable and see affects
-    var x, y;
-x = 5 + 6;
-y = x * 10;
-document.getElementById("first").innerHTML = y;
-
-function sayHi() {
-    var elemRef = document.getElementById("first");
-    elemRef.innerHTML = "goodbye";
-
-}
-
 //reveals changes with button selection on my html and css files
+
 function myFunctionreveal() {
         document.getElementById("jsbutton").innerHTML = "Oceanside, Oregon";
 }   
@@ -27,4 +16,3 @@ function myFunctionreveal() {
 
 
 
-    
