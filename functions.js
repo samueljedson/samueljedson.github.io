504f5d4bca52fb74d63d8b14647bd28326537ec1
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-25 18:36:54 -0700

    changes 8

--- functions.js
+++ functions.js
@@ -1,18 +1,8 @@
-//Commenting out alert
-//window.onload = function sayHi(){ alert(“Hello World of Javascript!”); } x = “goodbye world”  y=5
-//
-
-//reveals changes with button selection on my html and css files
-  
-function myFunctionlook() {
-        document.getElementById("jsbuttonstyle").style.color = "blue";
-}
-
 <script>
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Thelma and Louise";
+  var x = Math.floor((Math.random() * 100) + 0);
+  document.getElementById("demo").innerHTML = x;
 }
 </script>
-
 
 
