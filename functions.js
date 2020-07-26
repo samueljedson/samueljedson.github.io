ecb618936c90c2fca4b699825142897b196d6b70
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-25 19:33:16 -0700

    changes 17

--- functions.js
+++ functions.js
@@ -1,7 +1,3 @@
-function myFunction() {
-  var x = Math.floor((Math.random() * 100) + 0);
-  document.getElementById("demo").innerHTML = x;
-}
 
 function myFunctionlook() {
     document.getElementById("style").style.color = "purple";
