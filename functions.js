e4f3e704676019badf536d2a0c00b9de41cdc4e8
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-26 22:06:22 -0700

    changes blah blah

--- functions.js
+++ functions.js
@@ -15,3 +15,10 @@ function myform() {
     alert("Here's a hint for mine. I like salt and butter on it!");
   }
 
+var imagesArray = ["horse.jpg","zebra.jpg","bird.jpg"];
+function displayImage(){
+    var num = Math.floor(Math.random() * 4); 
+    document.canvas.src = imagesArray[num];
+}
+
+
