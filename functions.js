c368163a72d8638eda1292a46d7b292eec15d519
Samuel Edson <samueledson@Samuels-MacBook-Air.local>
2020-07-26 22:23:58 -0700

    fnl

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 function myFunctionGo() {
     document.getElementById("catfish").innerHTML = "Thelma and Louise";
 }
-
+//dunno why this failed so many times some browsers may not support?
 function myFunctionlook() {
     document.getElementById("style").style.color = "purple";
 }
@@ -14,7 +14,8 @@ function myFunction() {
 function myform() {
     alert("Here's a hint for mine. I like salt and butter on it!");
   }
-
+//attempt at uploading images for random selection when I ran it and inspected it the error read Failed to load resource: the server responded with a status of 404 ()
+//
 var imagesArray = ["horse.jpg","zebra.jpg","bird.jpg"];
 function displayImage(){
     var num = Math.floor(Math.random() * 4); 
